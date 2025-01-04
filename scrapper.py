fe295ece16efab7a57b2cdb40ca5955f51935d62
natokratos <darktimeangel@gmail.com>
2025-01-04 18:12:28 -0300

    Teste do scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,16 @@
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import requests
 import os
 import re
+import platform 
+import io
 
 class Scrapper:
 
@@ -12,16 +18,27 @@ class Scrapper:
         
         self.source_url = 'https://sistemaswebb3-listados.b3.com.br/indexPage/day/IBOV?language=pt-br'
 
-    def download_data(self):
+    def download_data(self, url):
         '''
         Realizar a requisição HTTP e retornar o conteúdo
         '''
 
         try:
+            if "Linux" in platform.system():
+                service = Service(executable_path="./geckodriver-linux")
+            elif "Windows" in platform.system():
+                service = Service(executable_path="./geckodriver.exe")
+            else:
+                service = Service(executable_path="./geckodriver")
+
             options = Options()
+            # options.headless = True
             options.add_argument("--headless")
-            driver = webdriver.Firefox(options=options)
-            driver.get(self.source_url)            
+            #options.add_argument(r"--binary_location C:\Users\darkt\Apps\FirefoxPortable\App\Firefox64")
+            # options.add_argument('--binary_location /usr/bin/firefox')
+            # options.binary_location=r"/usr/bin/firefox"
+            driver = webdriver.Firefox(service=service, options=options)
+            driver.get(url) 
             elems = driver.find_elements(By.XPATH,  "//*[contains(@href, 'indexPage/')]")
             #all_href = driver.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
             content = {}
@@ -32,6 +49,108 @@ class Scrapper:
                     content[elem.text.replace(' ','_').replace('-','').replace('.','').replace('ª','')] = elem.get_attribute('href')
             #print(all_href)
             driver.quit()
+            # response = requests.get()
+            # response.raise_for_status()
+
+            
+            # if response.content:
+            #     soup = BeautifulSoup(response.content, 'html.parser')
+            #     btn_download = soup.find('app-root')
+            #     print(f"btn_download {btn_download}")
+            #     if btn_download and 'href' in btn_download.attrs:
+            #         content = f"{self.source_url}/{btn_download['href']}"
+            # print(f"\n\ncontent{response.content}")
+            # print(f"\n\ncontent{response.json}")
+            #print(list(content))
+            return content
+        except requests.RequestException as e:
+            #file_name = url.split("=")[-1]
+            file_name = url.split("/")[-1]
+            print(f"options[file_name] {options[file_name]}")
+            if options and not options[file_name]:
+                file_name = f"{options[file_name]}_default.csv"
+            elif sub_options and not sub_options[file_name]:
+                file_name = f"{sub_options[file_name]}.csv"
+            print(f"file_name {file_name}")
+            pattern = re.compile(f"{file_name}[^ ]+.csv")
+            found = False
+            #print(f"pattern {pattern}")
+            for fp in os.listdir("src/database/temp_files/"):
+                match = pattern.match(fp)
+                #print(f"fp {fp} match {match}")
+                if match:
+                    file_name = f"src/database/temp_files/{str(match.group(0))}"
+                    print(f"O CSV correspondente [{file_name}] existe, tamanho [{os.path.getsize(file_name)}]")
+                    #response = BeautifulSoup(open(file_name, "rb"), 'lxml')
+                    #response = open(file_name, "rb")
+                    #print(f"response {response}")
+                    #return response
+                    found = True
+                    #break
+            if not found:
+                print(f'\nURL [{url}] \nERRO: [{e}]\n')
+
+            return None
+
+    def download_data1(self, url):
+        '''
+        Realizar a requisição HTTP e retornar o conteúdo
+        '''
+
+        try:
+            if "Linux" in platform.system():
+                service = Service(executable_path="./geckodriver-linux")
+            elif "Windows" in platform.system():
+                service = Service(executable_path="./geckodriver.exe")
+            else:
+                service = Service(executable_path="./geckodriver")
+
+            options = Options()
+            # options.headless = True
+            options.add_argument("--headless")
+            #options.add_argument(r"--binary_location C:\Users\darkt\Apps\FirefoxPortable\App\Firefox64")
+            # options.add_argument('--binary_location /usr/bin/firefox')
+            # options.binary_location=r"/usr/bin/firefox"
+            driver = webdriver.Firefox(service=service, options=options)
+            driver.get(url) 
+            
+            table = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "table.table-responsive-sm"))).get_attribute("outerHTML")
+            f = io.StringIO(table)
+            content = pd.read_html(f)[0]
+
+            # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="acceptCookieButton"]'))).click()
+
+            # pagination_list = WebDriverWait(driver, 20).until(EC.presence_of_element_located(driver.find_element(By.XPATH, "/html/body/app-root/app-theoretical-portfolio/div/div/div/form/div[2]/div/div[0]/div[1]/nav/pagination-controls/pagination-template/ul")))
+            pagination_list = WebDriverWait(driver, 20).until(EC.presence_of_element_located(driver.find_element(By.XPATH, "/html/body/app-root")))
+            # teste=WebDriverWait(driver, 20).until(EC.visibility_of_element_located(driver.find_element(By.CSS_SELECTOR, "ngx-pagination")))
+            # print(teste)
+            # pagination_list = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(By.CSS_SELECTOR, "ngx-pagination"))
+            print(f"pagination_list {pagination_list}")
+            pagination_numbers = pagination_list.find_elements(By.TAG_NAME, "a")
+            for page_number in pagination_numbers:
+                print(f"page_number {page_number}")
+                # Click on the pagination number
+                # WebDriverWait(driver, 20).until(EC.visibility_of(driver.find_element(By.CSS_SELECTOR, "div.backdrop")))
+                # WebDriverWait(driver, 20).until(EC.element_to_be_clickable("ngx-pagination")).click()
+                # page_number.click()            
+
+                table = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "table.table-responsive-sm"))).get_attribute("outerHTML")
+                f = io.StringIO(table)
+                content = content + pd.read_html(f)[0]
+
+            print(content)
+
+            # elems = driver.find_element(By.cssSelector("table.table-responsive-sm"));
+            # #all_href = driver.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+            # content = {}
+            # for elem in elems:
+            #     #if elem.get_attribute('href') not in content and not elem.get_attribute('href').endswith("indexPage/"):
+            #     print(f"XXXXXXXXX [{elem}]")
+            #     if not elem.get_attribute('href').endswith("indexPage/"):
+            #         #print(elem.get_attribute('href'))
+            #         content[elem.text.replace(' ','_').replace('-','').replace('.','').replace('ª','')] = elem.get_attribute('href')
+            # #print(all_href)
+            # driver.quit()
             # response = requests.get()
             # response.raise_for_status()
 
@@ -149,14 +268,46 @@ class Scrapper:
         
     #     return result
 
-    def download_csv(self, download_url, folder_path, file_name, options, sub_options):
+    def get_content(self, url):
+        '''
+        Realizar a requisição HTTP e retornar o conteúdo
+        '''
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.content
+        except requests.RequestException as e:
+            file_name = url.split("/")[-1]
+            # print(f"options[file_name] {options[file_name]}")
+            # if options and not options[file_name]:
+            #     file_name = f"{options[file_name]}_default.csv"
+            # elif sub_options and not sub_options[file_name]:
+            #     file_name = f"{sub_options[file_name]}.csv"
+            print(f"file_name {file_name}")
+            pattern = re.compile(f"{file_name}[^ ]+.csv")
+            found = False
+            #print(f"pattern {pattern}")
+            for fp in os.listdir("src/database/temp_files/"):
+                match = pattern.match(fp)
+                #print(f"fp {fp} match {match}")
+                if match:
+                    file_name = f"src/database/temp_files/{str(match.group(0))}"
+                    print(f"O CSV correspondente [{file_name}] existe, tamanho [{os.path.getsize(file_name)}]")
+                    found = True
+            if not found:
+                print(f'\nURL [{url}] \nERRO: [{e}]\n')
+
+            return None
+
+    def download_csv(self, download_url, folder_path, file_name):
 
         '''
         Realizar o download e escrita do CSV
         '''
 
-        print(f"Baixando {download_url} {folder_path} {file_name}")
-        content = self.get_content(download_url, options, sub_options)
+        print(f"Baixando {download_url} {folder_path}/{file_name}")
+        content = self.download_data1(download_url)
 
         if content:
 
@@ -165,6 +316,7 @@ class Scrapper:
 
             file_path = os.path.join(folder_path, file_name)
 
+            print(content)
             with open(file_path, 'wb') as file:
     
                 file.write(content)
@@ -177,15 +329,17 @@ class Scrapper:
         #               'ExpVinho.csv':'opt_06_subopt_01', 'ExpEspumantes.csv':'opt_06_subopt_02', 'ExpUva.csv':'opt_06_subopt_03', 'ExpSuco.csv':'opt_06_subopt_04'}
 
         print(f"Baixando os dados da B3 ...")
-        data= self.download_data()
+        data= self.download_data(self.source_url)
         #print(f"{data}\n")
 
         for d in data:
-            print(d)
             # opt = data['opt']
             # sub_opt = data['sub_opt'] if data['sub_opt'] else 'default'
             # download_url = data['download_url']
-            # file_name = f"{opt}_{sub_opt}.csv"
-            # file_path = 'src/database/temp_files'
-            # print(f"Download CSV {download_url} ...")
-            #self.download_csv(data, file_path, file_name, options, sub_options)
+            if d != "Download":
+                print(f"[{d}] [{data[d]}]")
+                file_name = f"{d}.csv"
+                file_path = './temp_files'
+                print(f"Download CSV {d} ...")
+                self.download_csv(data[d], file_path, file_name)
+                break
